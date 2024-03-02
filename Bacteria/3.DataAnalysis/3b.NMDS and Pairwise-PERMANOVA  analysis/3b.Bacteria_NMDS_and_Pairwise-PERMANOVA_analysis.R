b56b949858f8a8c764e5cbad600fa5c4048e551d
Fotisbs <87061822+Fotisbs@users.noreply.github.com>
2024-03-02 02:17:34 +0200

    Add files via upload

---
+++ Bacteria/3.DataAnalysis/3b.NMDS and Pairwise-PERMANOVA  analysis/3b.Bacteria_NMDS_and_Pairwise-PERMANOVA_analysis.R
@@ -0,0 +1,23 @@
+##nmds##
+MUST_Bacteria_Vidiano_2020_Merged_100
+
+ord.nmds.bray1 <- ordinate(MUST_Bacteria_Vidiano_2020_Merged_100, method="NMDS", distance="bray")
+
+
+plot_ordination(MUST_Bacteria_Vidiano_2020_Merged_100, ord.nmds.bray1, color="vinification", shape ="", label = "", title=paste("VIDIANO Bacteria 2020")) + geom_point(size = 4) + scale_color_manual(values = mycols) + theme_classic()
+
+plot_ordination(MUST_Bacteria_Vidiano_2020_Merged_100, ord.nmds.bray1, color="stage", shape ="", label = "", title=paste("VIDIANO Bacteria 2020")) + geom_point(size = 4) + scale_color_manual(values = mycols) + theme_classic()
+
+
+
+
+plot_ordination(MUST_Bacteria_Vidiano_2020_Merged_100, ord.nmds.bray1, color="vinification", shape ="", label = "stage", title=paste("VIDIANO Bacteria 2020")) + geom_point(size = 4) + scale_color_manual(values = "#66A61E","#E6AB02","#A6761D","#666666","#E41A1C")
+
+plot_ordination(MUST_Bacteria_Vidiano_2020_Merged_100, ord.nmds.bray1, color="stage", shape ="vinification", label = "stage", title=paste("VIDIANO Bacteria 2020")) + geom_point(size = 4) + scale_color_manual(values = mycols)
+
+##pairwise permanova (sto RA% object)##
+library(pairwiseAdonis)
+
+mycmpfactor_MUST_Bacteria_Vidiano_2020_Merged_100 <- interaction(data.frame(MUST_Bacteria_Vidiano_2020_Merged_100@sam_data)$vinification, data.frame(MUST_Bacteria_Vidiano_2020_Merged_100@sam_data)$vinification) 
+
+mympairwiseperm_MUST_Bacteria_Vidiano_2020_Merged_100 <- pairwise.adonis(MUST_Bacteria_Vidiano_2020_Merged_100@otu_table, mycmpfactor_MUST_Bacteria_Vidiano_2020_Merged_100, sim.function = "vegdist", sim.method = "bray", p.adjust.m = "BH")##variety vs variety
